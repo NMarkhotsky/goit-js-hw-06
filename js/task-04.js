77bfd5b4e5ffb91281fc16e50f898e4b7ada29bc
Nikolay Markhotskyi <marhotsky@gmail.com>
2023-01-12 00:34:03 +0200

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -4,10 +4,10 @@ const valueEl = document.querySelector('#value');
 
 let counterValue = 0;
 
-const onDecrementCounter = decrementBtn.addEventListener('click', () => {
+decrementBtn.addEventListener('click', () => {
   valueEl.textContent = counterValue -= 1;
 });
 
-const onIncrementCounter = incrementBtn.addEventListener('click', () => {
+incrementBtn.addEventListener('click', () => {
   valueEl.textContent = counterValue += 1;
 });
