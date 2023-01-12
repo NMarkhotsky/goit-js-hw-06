601625ab916a349a40b037d40a3be25bb4db46ae
Nikolay Markhotskyi <marhotsky@gmail.com>
2023-01-12 18:13:05 +0200

    update

--- js/task-07.js
+++ js/task-07.js
@@ -2,15 +2,17 @@ const inputEl = document.querySelector('#font-size-control');
 const textEl = document.querySelector('#text');
 
 //* 1 спосіб
-inputEl.addEventListener('input', changeSizeText);
-
-function changeSizeText() {
-  textEl.style.fontSize = inputEl.value + 'px';
-}
+textEl.style.fontSize = `${inputEl.value}px`;
+inputEl.addEventListener(
+  'input',
+  (event) => (textEl.style.fontSize = `${event.currentTarget.value}px`)
+);
 
 //* 2 спосіб
+// textEl.style.fontSize = inputEl.value + 'px';
 
-// inputEl.addEventListener(
-//   'input',
-//   () => (textEl.style.fontSize = inputEl.value + 'px')
-// );
+// inputEl.addEventListener('input', OnChangeSizeText);
+
+// function OnChangeSizeText() {
+//   textEl.style.fontSize = inputEl.value + 'px';
+// }
